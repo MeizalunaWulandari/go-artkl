d1dc6ed90b15fe47fa6ead91953cfa133ba67dc1
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-05-09 12:40:02 +0800

    update UserRepository.go

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -11,6 +11,7 @@ type UserRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, user domain.User) domain.User
 	Update(ctx context.Context, tx *sql.Tx, user domain.User) domain.User
 	Delete(ctx context.Context, tx *sql.Tx, user domain.User)
-	FindByUsername(ctx context.Context, tx *sql.Tx, username string) domain.User
+	FindByUsername(ctx context.Context, tx *sql.Tx, username string) (domain.User, error)
+	FindById(ctx context.Context, tx *sql.Tx, userId int) (domain.User, error)
 	FindAll(ctx context.Context, tx *sql.Tx, user domain.User) []domain.User
 }
